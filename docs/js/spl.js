c586a10b496e4f2938652b2eb1e3e34075e0bc0d
wjw465150 <wjw465150@gmail.com>
2020-10-14 16:40:52 +0800

    顶尖、高手、优秀、中级、合格、菜鸟 <60，60～70，70～80， 80～90， 90～100 , >100

--- docs/js/spl.js
+++ docs/js/spl.js
@@ -51,21 +51,25 @@ $(document).ready(function () {
         var baimipeisu = parseInt((num3*100/num1*1.05)/60)+"分"+parseInt(num3*100/num1*1.05)%60;
         $('#tb_9').textbox('setValue',baimipeisu);
 
-        //<60，60～70，70～80，80～90，>90
+        //顶尖、高手、优秀、中级、合格、菜鸟
+        //<60，60～70，70～80， 80～90， 90～100 , >100
 
         var qujian = num3*100/num1*1.05
         if(swolf <= 60) {
-            $('#tb_10').text('大神');
+            $('#tb_10').text('顶尖');
             $('#tb_10').css('background-color','green');
         } else if(swolf <= 70) {
-            $('#tb_10').text('优秀');
+            $('#tb_10').text('高手');
             $('#tb_10').css('background-color','yellow');
         } else if(swolf <= 80) {
-            $('#tb_10').text('良好');
+            $('#tb_10').text('优秀');
             $('#tb_10').css('background-color','#ffab2e');
         } else if(swolf <= 90) {
-            $('#tb_10').text('及格');
+            $('#tb_10').text('中级');
             $('#tb_10').css('background-color','blue');
+        } else if(swolf <= 100) {
+            $('#tb_10').text('合格');
+            $('#tb_10').css('background-color','#ffa8a8');
         } else {
             $('#tb_10').text('菜鸟');
             $('#tb_10').css('background-color','red');
