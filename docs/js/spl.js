b5042da7621ec3fa5120462126d8c31cab07db82
wjw465150 <wjw465150@gmail.com>
2020-10-15 11:33:49 +0800

    改成TAB页风格!

--- docs/js/spl.js
+++ docs/js/spl.js
@@ -1,3 +1,19 @@
+/* js 数字类型取整方法
+let num = 1.75
+
+1.向上取整
+let result = Math.round(num)
+console.log(result) // 结果为 2
+
+2.四舍五入
+let result = Math.ceil(num)
+console.log(result) // 结果为 2
+
+3.向下取整
+let result = Math.floor(num)
+console.log(result) // 结果为 1
+ */
+
 //四舍五入保留2位小数（不够位数，则用0替补）
 function keepTwoDecimalFull(num) {
     var result = parseFloat(num);
@@ -23,76 +39,75 @@ $(document).ready(function () {
         closable: false
     });
 
-
     $('#btn_SPL').bind('click', function () {
-        var num1=$('#tb_1').numberbox('getValue');
-        var num2=$('#tb_2').numberbox('getValue');
-        var huaju=keepTwoDecimalFull((num1-5)/num2);
+        var num1 = $('#tb_1').numberbox('getValue');
+        var num2 = $('#tb_2').numberbox('getValue');
+        var huaju = keepTwoDecimalFull((num1 - 5) / num2);
 
-        var num3=$('#tb_3').numberbox('getValue');
-        var jiepai=keepTwoDecimalFull((num3-3)/num2);
+        var num3 = $('#tb_3').numberbox('getValue');
+        var jiepai = keepTwoDecimalFull((num3 - 3) / num2);
 
-        $('#tb_4').textbox('setValue',huaju);
-        $('#tb_5').textbox('setValue',jiepai);
+        $('#tb_4').textbox('setValue', huaju);
+        $('#tb_5').textbox('setValue', jiepai);
 
-        var huashuilu = Math.round(60/jiepai);
-        $('#tb_6').textbox('setValue',huashuilu);
+        var huashuilu = Math.round(60 / jiepai);
+        $('#tb_6').textbox('setValue', huashuilu);
 
-        var swolf = num2*1+num3*1;
-        $('#tb_7').textbox('setValue',swolf);
+        var swolf = num2 * 1 + num3 * 1;
+        $('#tb_7').textbox('setValue', swolf);
 
-        var yousu = keepTwoDecimalFull(num1/num3);
-        $('#tb_8').textbox('setValue',yousu);
+        var yousu = keepTwoDecimalFull(num1 / num3);
+        $('#tb_8').textbox('setValue', yousu);
 
-        var baimipeisu = parseInt((num3*100/num1*1.05)/60)+"分"+parseInt(num3*100/num1*1.05)%60+"秒";
-        $('#tb_9').textbox('setValue',baimipeisu);
+        var baimipeisu = parseInt((num3 * 100 / num1 * 1.05) / 60) + "分" + parseInt(num3 * 100 / num1 * 1.05) % 60 + "秒";
+        $('#tb_9').textbox('setValue', baimipeisu);
 
         //顶尖、高手、优秀、中级、合格、菜鸟
         //<60，60～70，70～80， 80～90， 90～100 , >100
-        if(swolf <= 60) {
+        if (swolf <= 60) {
             $('#tb_10').text('顶尖');
-            $('#tb_10').css('color','white');
-            $('#tb_10').css('background-color','green');
-        } else if(swolf <= 70) {
+            $('#tb_10').css('color', 'white');
+            $('#tb_10').css('background-color', 'green');
+        } else if (swolf <= 70) {
             $('#tb_10').text('高手');
-            $('#tb_10').css('color','black');
-            $('#tb_10').css('background-color','yellow');
-        } else if(swolf <= 80) {
+            $('#tb_10').css('color', 'black');
+            $('#tb_10').css('background-color', 'yellow');
+        } else if (swolf <= 80) {
             $('#tb_10').text('优秀');
-            $('#tb_10').css('color','black');
-            $('#tb_10').css('background-color','#ffab2e');
-        } else if(swolf <= 90) {
+            $('#tb_10').css('color', 'black');
+            $('#tb_10').css('background-color', '#ffab2e');
+        } else if (swolf <= 90) {
             $('#tb_10').text('中级');
-            $('#tb_10').css('color','white');
-            $('#tb_10').css('background-color','blue');
-        } else if(swolf <= 100) {
+            $('#tb_10').css('color', 'white');
+            $('#tb_10').css('background-color', 'blue');
+        } else if (swolf <= 100) {
             $('#tb_10').text('合格');
-            $('#tb_10').css('color','black');
-            $('#tb_10').css('background-color','#ffa8a8');
+            $('#tb_10').css('color', 'black');
+            $('#tb_10').css('background-color', '#ffa8a8');
         } else {
             $('#tb_10').text('菜鸟');
-            $('#tb_10').css('color','white');
-            $('#tb_10').css('background-color','red');
+            $('#tb_10').css('color', 'white');
+            $('#tb_10').css('background-color', 'red');
         }
 
     });
 
     $('#btn_HC').bind('click', function () {
-        var num1=$('#tb_21').numberbox('getValue');
-        var num2=$('#tb_22').numberbox('getValue');
-        var num3=$('#tb_23').numberbox('getValue');
+        var num1 = $('#tb_21').numberbox('getValue');
+        var num2 = $('#tb_22').numberbox('getValue');
+        var num3 = $('#tb_23').numberbox('getValue');
 
-        var huaciqujian = parseInt(((num1-5)/num2)/0.55)+ " 到 "+Math.round(((num1-5)/num2)/0.7);
-        $('#tb_31').textbox('setValue',huaciqujian);
+        var huaciqujian = parseInt(((num1 - 5) / num2) / 0.55) + " 到 " + Math.round(((num1 - 5) / num2) / 0.7);
+        $('#tb_31').textbox('setValue', huaciqujian);
 
-        var huaciHJ = keepTwoDecimalFull(num2*0.55)+ " 到 "+keepTwoDecimalFull(num2*0.7);
-        $('#tb_32').textbox('setValue',huaciHJ);
+        var huaciHJ = keepTwoDecimalFull(num2 * 0.55) + " 到 " + keepTwoDecimalFull(num2 * 0.7);
+        $('#tb_32').textbox('setValue', huaciHJ);
 
-        var hc_1 = parseInt(((num1-5)/num2)/0.55);
-        var hc_2 = Math.round(((num1-5)/num2)/0.7);
+        var hc_1 = parseInt(((num1 - 5) / num2) / 0.55);
+        var hc_2 = Math.round(((num1 - 5) / num2) / 0.7);
 
-        var jiepaifanwei = keepTwoDecimalFull((num3-3)/hc_1)+ " 到 "+keepTwoDecimalFull((num3-3)/hc_2);
-        $('#tb_33').textbox('setValue',jiepaifanwei);
+        var jiepaifanwei = keepTwoDecimalFull((num3 - 3) / hc_1) + " 到 " + keepTwoDecimalFull((num3 - 3) / hc_2);
+        $('#tb_33').textbox('setValue', jiepaifanwei);
     });
 });
 
